f74799d70bd5db03d28fb1e0bf3fd144dfd06562
IsmaelMatiz <ismaelmatiz@gmail.com>
2023-08-20 12:08:15 -0500

    menu principal hecho

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
         public static void main(String[] args) {
-            System.out.println("Hola Mundo!");
+
+                ImageIcon logo = new ImageIcon("src/main/java/images/logo.png");
+
+                //Calculator Options
+                String [] converters = new String[]{"Convertidor de moneda","Convertidor de Binario a decimal"};
+                JOptionPane.showInputDialog(null,"Elije el convertidor a udar",
+                        "Menu Principal",
+                        JOptionPane.QUESTION_MESSAGE, logo,converters,converters[0]
+                        );
         }
 }
